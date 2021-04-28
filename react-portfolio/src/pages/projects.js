393c5657414d5c87542e2e045add65604145d211
Jody Brzovski <jody.brzovski@gmail.com>
2021-04-28 16:01:40 -0400

    ProjectCard Component

--- react-portfolio/src/pages/projects.js
+++ react-portfolio/src/pages/projects.js
@@ -1,12 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import projectArray from "./friends.json";
+import { Col, Row, Container } from "../components/Grid";
+import ProjecrCard from "../components/ProjecrCard";
 
-function Projects() {
-  const [projects, setProjects] = useState([]);
+class Projects extends Component {
+  state = {
+    projectArray
+  };
 
-useEffect(() => {
-  setProjects(projectArray);
-}, []);
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            {this.state.projectArray.map(project => (
+              <ProjecrCard
+                image-link={project.image-link}
+                name={project.name}
+                image={project.image}
+                description={project.description}
+                repo-link={project.repo-link}
+
+              />
+            ))}
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 
 }
 
--- react-portfolio/utils/ProjectContext.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ProjectContext = React.createContext({
-  projects: [],
-});
-
-export default ProjectContext;
