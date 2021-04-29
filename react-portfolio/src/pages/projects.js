7539d7fe4d3871ada35a96cf8b05ac6ba5798d8f
Jody Brzovski <jody.brzovski@gmail.com>
2021-04-29 17:09:48 -0400

    Routing functional, worked out various bugs on npm start

--- react-portfolio/src/pages/projects.js
+++ react-portfolio/src/pages/projects.js
@@ -1,34 +1,37 @@
-import React, { Component } from "react";
-import projectArray from "./friends.json";
+import React, { useState, useEffect } from "react";
+import projectArray from "../projects.json";
 import { Col, Row, Container } from "../components/Grid";
-import ProjecrCard from "../components/ProjecrCard";
+import ProjectCard from "../components/ProjectCard";
 
-class Projects extends Component {
-  state = {
-    projectArray
+function Projects() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    loadProjects()
+  }, [])
+
+  // Loads all books and sets them to books
+  function loadProjects() {
+    setProjects(projectArray)
   };
 
-  render() {
-    return (
+  return(
       <Container fluid>
         <Row>
           <Col size="md-6">
-            {this.state.projectArray.map(project => (
-              <ProjecrCard
-                image-link={project.image-link}
+            {projects.map(project => (
+              <ProjectCard key={project.id}
+                imageLink={project.imageLink}
                 name={project.name}
                 image={project.image}
                 description={project.description}
-                repo-link={project.repo-link}
-
+                repoLink={project.repoLink}
               />
             ))}
           </Col>
         </Row>
       </Container>
-    )
+    );
   }
-
-}
 
 export default Projects;
